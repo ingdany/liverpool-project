36fe074f6b6e9efb66d65516fd4085ab67856303
Daniel Pérez <ing.dany@gmail.com>
2024-12-06 09:13:38 -0800

    Integrate Boilerplate

---
+++ src/main/java/config/env_target.java
@@ -0,0 +1,8 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+
+public class env_target {
+    public static WebDriver driver;
+    public static String baseUrl = "https://www.liverpool.com.mx/tienda/home";
+}
